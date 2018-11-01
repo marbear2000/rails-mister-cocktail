ea718f2491ef009048ad8522d689bf48cd08280a
Mary Healy <maryhealy1978@gmail.com>
2018-11-01 17:52:36 +0100

    mr cocktail without front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Ingredient.delete_all
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+file = open(url).read
+
+data = JSON.parse(file)
+
+# p data_hash.first[1].class
+
+# ingredients = []
+
+data.first[1].each do |i|
+  Ingredient.create(name: i["strIngredient1"])
+end
+
+# p ingredient
+
